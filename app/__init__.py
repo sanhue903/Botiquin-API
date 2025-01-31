e34ae48336320261ad7377e44120741281e99e2d
sanhue_903 <sesanhueza2019@udec.cl>
2025-01-31 01:01:02 -0300

    fix/url main view

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_class: Config = DevelopmentConfig):
         
         # Register blueprints
         app.register_blueprint(error_bp)
-        app.register_blueprint(main_bp)
+        app.register_blueprint(main_bp, url_prefix='/')
         app.register_blueprint(auth_bp, url_prefix='/auth')
         app.register_blueprint(mobile_app_bp, url_prefix='/apps')
         app.register_blueprint(score_bp, url_prefix='/apps/<app_id>/students')
